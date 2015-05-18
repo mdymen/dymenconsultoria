9d6a2e537ee6b2f7fce7679b65f2fc7e923a7046
mdymen <msn@dymenstein.com>
2015-05-18 15:22:12 -0300

    dymen consultoria

---
+++ src/main/java/com/dymenconsultoria/classes/Cursos.java
@@ -0,0 +1,13 @@
+package com.dymenconsultoria.classes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cursos {
+
+	public Cursos() {}
+	
+	public String hola() {
+		return "HOLA";
+	}
+}
